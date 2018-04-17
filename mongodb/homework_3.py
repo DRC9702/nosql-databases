d2e89d247b0eafdb153d455edf554c8bf8f6705a
DRC9702 <david.rincon.cruz@gmail.com>
2018-04-17 14:44:41 -0400

    Almost done'

--- mongodb/homework_3.py
+++ mongodb/homework_3.py
@@ -3,22 +3,22 @@ from pymongo import MongoClient
 from pprint import pprint
 
 client = MongoClient()
-print(client.database_names())
 database = client.movies
-print(database.collection_names())
 collection = database.movies
-print(collection)
 woop = collection.find({}).count()
-print(woop)
+
 # A. Find movies with rating: 'NOT RATED' and update to "Pending rating"
+print("PART A")
 cursor = collection.find({"rated": "NOT RATED"})
-print(cursor.count())
+#print(cursor.count())
 for document in cursor:
 	collection.update({"_id":document["_id"]},{"rated": "Pending rating"})
-print(cursor.count())
+print("\t ...All 'not rated' ratings updated to 'pending rating'")
+#print(cursor.count())
 
 # B. Insert movie into database - inserting the movie Pandas(2018)
 # I had to make up the values for id and votes
+print("PART B")
 if collection.find({"title": "pandas"}).count()==0:
 	doc = {"title": "pandas",
 		"year": 2018,
@@ -32,15 +32,17 @@ for document in cursor:
 	pprint(document)
 
 #C. Use aggregation to find total number of genre:short movies
+print("Part C")
 pipeline = [{"$unwind": "$genres"}, {"$group": {"_id": "short", "count": {"$sum": 1}}}]
 agg = collection.aggregate(pipeline)
 pprint(list(agg))
 
 
 #D. Use aggregation to find number of movies made in Mexico in 1995 with rated:"Pending rating"
+print("PART D")
 pipeline = [	{"$match": {"year": 1995}},
 		{"$unwind": "$countries"},
-		{"$group": {"_id": {"countries": "Mexico"}, "count": {"$sum": 1}}}]
+		{"$group": {"_id": {"countries": "Mexico", "rated": "Pending rating"}, "count": {"$sum": 1}}}]
 #		{"$group": {"_id": {"countries": "Mexico", "year": "1995"}, "count": {"$sum": 1}}}]
 
 agg = collection.aggregate(pipeline)
